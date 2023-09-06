ed3bdbdbe6223b2b1952e7afd3e307f6f886598c
Rajan <rajanmasih154@gmail.com>
2023-09-06 17:54:07 +0530

    Adding routes for pages

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const menuIcon = document.getElementById("menuIcon");
 const xMarkIcon = document.getElementById("xMarkIcon");
 const options = document.getElementById("options2");
+const viewButton = document.querySelector(".view");
+const home = document.querySelector(".home");
+const work = document.querySelector(".work");
+
+home.addEventListener("click", openHomeFile);
+work.addEventListener("click", openProjectFile);
+viewButton.addEventListener("click", openProjectFile);
+
+// For Home
+function openHomeFile() {
+    window.open("./index.html", "_self");
+}
+
+// For Project
+function openProjectFile() {
+    window.open("./pages/project.html", "_self");
+}
 
 menuIcon.addEventListener("click", () => {
     menuIcon.style.display = "none";
